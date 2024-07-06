ed1ed194504aeaf06a787952f1911e73c65f4f9f
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2024-07-06 23:48:23 +0900

    [update] path alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '~/', replacement: `${__dirname}/` },
+      { find: '@/', replacement: `${__dirname}/app/` },
+    ]
+  },
   plugins: [
     remix({
       ssr: false,
