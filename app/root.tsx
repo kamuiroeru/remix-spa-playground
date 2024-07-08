ab024b6ee93ff4f535583a52c1c9d4a9d9f0d85e
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2024-07-08 22:23:48 +0900

    [update] DarkLight Mode Save

--- app/root.tsx
+++ app/root.tsx
@@ -6,12 +6,22 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 import './tailwind.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { clsx } from 'clsx/lite';
 import { LightDarkSwitch } from './components/ui/LightDarkSwitch';
+import { getIsDarkMode, saveIsDarkMode } from './utils/darkMode';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const updateIsDarkMode = (checked: boolean) => {
+    saveIsDarkMode(checked);
+    setIsDarkMode(checked);
+  };
+
+  useEffect(() => {
+    updateIsDarkMode(getIsDarkMode());
+  }, [setIsDarkMode]);
 
   return (
     <html lang="en">
@@ -29,10 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       >
         <header className="flex w-full min-w-[1024px] items-center gap-5 bg-blue-500 p-4 text-white">
           <p className="text-3xl">Remix App</p>
-          <LightDarkSwitch
-            defaultChecked={isDarkMode}
-            onChange={setIsDarkMode}
-          />
+          <LightDarkSwitch checked={isDarkMode} onChange={updateIsDarkMode} />
         </header>
         <main className="flex-1 bg-gray-50 dark:bg-gray-800">{children}</main>
         <ScrollRestoration />
