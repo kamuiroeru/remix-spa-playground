6ffaef47ebf12cd63c56d4d32898205a4d36bfbc
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2024-07-08 23:33:28 +0900

    [add] Switch

---
+++ app/components/ui/Switch/index.stories.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Presentation } from './Presentation';
+import type { Meta, StoryObj } from '@storybook/react';
+import { BASIC_COLORS, BasicColor } from '@/schema/color';
+
+type Props = {
+  onColor: BasicColor;
+};
+const PresentationMock = ({ onColor }: Props) => {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <Presentation checked={checked} onColor={onColor} onChange={setChecked} />
+  );
+};
+
+type Story = StoryObj<typeof PresentationMock>;
+
+export const Default: Story = {
+  args: {
+    onColor: 'blue',
+  },
+  argTypes: {
+    onColor: {
+      options: BASIC_COLORS,
+      control: 'radio',
+    },
+  },
+};
+
+export default {
+  title: 'ui/Switch',
+  component: PresentationMock,
+} satisfies Meta<typeof PresentationMock>;
