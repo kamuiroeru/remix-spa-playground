4ab150d799e63b9caa481281bf883ae7fc24e91c
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2024-07-08 23:11:23 +0900

    [refactor]

--- app/components/ui/Button/all.stories.tsx
+++ app/components/ui/Button/all.stories.tsx
@@ -1,6 +1,7 @@
 import { Icon } from '@iconify/react';
-import { buttonColors, buttonTypes, Presentation } from './Presentation';
+import { buttonTypes, Presentation } from './Presentation';
 import type { Meta, StoryObj } from '@storybook/react';
+import { BASIC_COLORS } from '@/schema/color';
 
 type Props = {
   text: string;
@@ -8,7 +9,7 @@ type Props = {
 const PresentationMock = ({ text }: Props) => (
   <div className="grid grid-cols-8 gap-4">
     {buttonTypes.map((type) =>
-      buttonColors.map((color) => (
+      BASIC_COLORS.map((color) => (
         <Presentation
           key={`Button-${type}-${color}`}
           text={text}
