edad102add49abd54f839524c1f863df588a781b
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2024-07-13 14:01:50 +0900

    [update] format on save complete.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -70,6 +70,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:import/recommended',
         'plugin:import/typescript',
+        'prettier',
       ],
       rules: {
         '@typescript-eslint/no-unused-vars': 'off',
